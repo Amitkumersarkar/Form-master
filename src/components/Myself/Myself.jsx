caf94bbb1dbf03e318400763b58d02cfe63dd668
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-06-04 23:20:34 +0600

    props driling file

--- src/components/Myself/Myself.jsx
+++ src/components/Myself/Myself.jsx
@@ -1,9 +1,11 @@
+import Wife from "../Wife/Wife";
 
 const Myself = () => {
 
     return (
         <div>
             <h2>Me</h2>
+            <Wife></Wife>
         </div>
     );
 };
---
+++ src/components/Wife/Wife.jsx
@@ -0,0 +1,11 @@
+
+const Wife = () => {
+
+    return (
+        <div>
+            <h2>Wife</h2>
+        </div>
+    );
+};
+
+export default Wife;
